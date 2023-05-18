17ec5d52ea3281a8e2e28b08f68135c7c8093595
barizaarfadillah <arfadillah.abqariy@gmail.com>
2023-05-18 22:37:34 +0700

    update

--- forgot-password-karyawan.php
+++ forgot-password-karyawan.php
@@ -1,7 +1,7 @@
 <?php
-require_once 'controllers/C_Login-Karyawan.php';
+require_once 'controllers/C_LupaPassword-Karyawan.php';
 
-$Login = new Login();
+$Forgot = new Forgot();
 
 
 
@@ -9,7 +9,6 @@ mysqli_report (MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -27,29 +26,29 @@ error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
             <div class="row" style="box-shadow: 5px 5px 10px 1px rgba(0, 0, 0, 0.2); background: #fff; border-radius: 10px; height: 580px; width: 900px;">
                 <div class="col-md-5 side-image d-flex align-items-center justify-content-center">
                     <!-------Image-------->
-                    <img src="assets/img/bro.png" alt="">
+                    <img src="assets/img/bro2.png" alt="">
                 </div>
                 <div class="col-md-7 right ">
                      <div class="input-box">
-                        <header>Login</header>
-                        <p>Masukkan email dan password anda</p>
-                        <!-- echo $msg; ?> -->
+                        <header>Lupa Password</header>
+                        <p>Masukkan email anda untuk melakukan verifikasi</p>
                         <form action="" method="post">
-                            <input type="email" class="email" name="email" placeholder="Email">
-                            <input type="password" class="password" name="password" placeholder="Password" style="margin-bottom: 2px;">
-                            <p class="d-flex justify-content-end"><a href="forgot-password-karyawan.php" style="margin-bottom: -5px; display: block; text-align: right;">Forgot Password?</a></p>
-                            <button name="login" name="login" class="btn" type="login">Login</button>
+                            <input type="email" class="email" name="email" placeholder="Email" required>
+                            <button name="submit" class="btn" type="submit">Reset Password</button>
                         </form>
+                        <div class="social-icons text-center">
+                            <p>kembali ke <a href="index.php">login</a>.</p>
+                        </div>
                      </div>
                 </div>
             </div>
         </div>
     </div>
-    <script src="js/bootsrap.bundle.js"></script>
-    <?php
-if(isset($_POST["login"])) {
-    $Login->login();
-  }
-    ?>
+    <script src="assets/js/bootsrap.bundle.js"></script>
 </body>
 </html>
+<?php
+if(isset($_POST["submit"])) {
+    $Forgot->forgot();
+  }
+?>
